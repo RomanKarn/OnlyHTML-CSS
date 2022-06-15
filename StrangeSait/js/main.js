2112ed8b92c9a8ff9cf91a0194985babd6d61c3c
RomanKarn <WorkKarn@yandex.ru>
2022-06-15 12:32:13 +0700

    StrangeSait

---
+++ StrangeSait/js/main.js
@@ -0,0 +1,10 @@
+$(function()
+{
+    $('.slider_inner, .news_slider-inner').slick({
+        nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false
+    })
+
+    $('input, select').styler();
+})
